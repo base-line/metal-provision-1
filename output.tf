95aacf99b2060132d8f4c302d2d714eb629ce965
Andrei Popa <andrei.popa@eu.equinix.com>
2021-10-20 15:27:38 +0200

    add new example

--- output.tf
+++ output.tf
@@ -101,3 +101,56 @@ output "state_userdata" {
   description = "device state"
   value       = module.device_user_data.state
 }
+
+// device with Elastic IP
+
+output "access_private_ipv4_userdata_ip" {
+  description = "private ipv4"
+  value       = module.device_ip.access_private_ipv4
+}
+
+output "access_public_ipv4_userdata_ip" {
+  description = "public ipv4"
+  value       = module.device_ip.access_public_ipv4
+}
+
+output "access_public_ipv6_userdata_ip" {
+  description = "public ipv6"
+  value       = module.device_ip.access_public_ipv6
+}
+
+output "created_userdata_ip" {
+  description = "when it was created"
+  value       = module.device_ip.created
+}
+
+output "hostname_userdata_ip" {
+  description = "hostname"
+  value       = module.device_ip.hostname
+}
+
+output "id_userdata_ip" {
+  description = "device id"
+  value       = module.device_ip.id
+}
+
+output "operating_system_userdata_ip" {
+  description = "device os"
+  value       = module.device_ip.operating_system
+}
+
+output "ssh_key_ids_userdata_ip" {
+  description = "device ssh key"
+  value       = module.device_ip.ssh_key_ids
+}
+
+output "root_password_userdata_ip" {
+  description = "device root password"
+  value       = module.device_ip.root_password
+  sensitive   = true
+}
+
+output "state_userdata_ip" {
+  description = "device state"
+  value       = module.device_ip.state
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "project_id" {
   type        = string
   default     = ""
 }
+
+variable "quantity" {
+  description = "quantity ip"
+  type        = string
+  default     = ""
+}
+
+variable "facility" {
+  description = "facility"
+  type        = string
+  default     = ""
+}
+
+variable "description" {
+  description = "description"
+  type        = string
+  default     = ""
+}
